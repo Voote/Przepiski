4620664814519e79187842f23516216e8958d388
Voote <kszygy@gmail.com>
2022-06-30 10:04:20 +0100

    initial useref

--- src/components/templates/MainTemplate.js
+++ src/components/templates/MainTemplate.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { useQuery } from 'graphql-hooks';
 import { MenuContext } from 'hooks/userContext';
 import { ModalContext } from 'hooks/userContext';
